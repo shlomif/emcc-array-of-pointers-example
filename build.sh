138c71847735d954b7b7cefe581b3363cee008bc
Shlomi Fish <shlomif@shlomifish.org>
2021-12-24 11:26:47 +0200

    add exports

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ rm -rf dist/
 mkdir -p dist
 objext=bc
 emcc -O3 src/c_array_strings.c src/main.cpp src/myfunc.cpp src/myclass.cpp -Iinclude -o dist/mysqrt."$objext"
-emcc -O3 dist/mysqrt."$objext" -o dist/mysqrt.js -s EXPORTED_FUNCTIONS='["_array_print", "_array_set", "_gen_array", "_mysqrtC", "_mysqrtArrayC", "_createMyClassC", "_deleteMyClassC", "_setMyClassValueC", "_getMyClassAnswerC"]' --memory-init-file 0 --pre-js  wrap/pre.js --post-js wrap/post.js
+emcc -O3 dist/mysqrt."$objext" -o dist/mysqrt.js -s EXPORTED_FUNCTIONS='["_array_print", "_array_set", "_gen_array", "_mysqrtC", "_mysqrtArrayC", "_createMyClassC", "_deleteMyClassC", "_setMyClassValueC", "_getMyClassAnswerC"]' -s EXPORTED_RUNTIME_METHODS="['allocate', 'cwrap', 'getValue', 'intArrayFromString', 'setValue', 'ALLOC_STACK', 'FS', 'UTF8ToString']" --memory-init-file 0 --pre-js  wrap/pre.js --post-js wrap/post.js
 export EMCC_DEBUG=""
