fa2df5a1c5be35778e4d39372837ca2f41570d08
Shlomi Fish <shlomif@shlomifish.org>
2021-12-24 11:08:15 +0200

    update to emcc tot

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+set -e -x
 export EMCC_DEBUG=1
+rm -rf dist/
 mkdir -p dist
-rm -r dist/*
-emcc -O3 src/main.cpp src/myfunc.cpp src/myclass.cpp -Iinclude -o dist/mysqrt.bc
-emcc -O3 dist/mysqrt.bc -o dist/mysqrt.js -s EXPORTED_FUNCTIONS='["_mysqrtC", "_mysqrtArrayC", "_createMyClassC", "_deleteMyClassC", "_setMyClassValueC", "_getMyClassAnswerC"]' --memory-init-file 0 --pre-js  wrap/pre.js --post-js wrap/post.js
+objext=bc
+emcc -O3 src/main.cpp src/myfunc.cpp src/myclass.cpp -Iinclude -o dist/mysqrt."$objext"
+emcc -O3 dist/mysqrt."$objext" -o dist/mysqrt.js -s EXPORTED_FUNCTIONS='["_mysqrtC", "_mysqrtArrayC", "_createMyClassC", "_deleteMyClassC", "_setMyClassValueC", "_getMyClassAnswerC"]' --memory-init-file 0 --pre-js  wrap/pre.js --post-js wrap/post.js
 export EMCC_DEBUG=""
