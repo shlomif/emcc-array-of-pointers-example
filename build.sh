24966f9b7bca6df060c4767845fabad14798c0a5
Shlomi Fish <shlomif@shlomifish.org>
2024-04-04 08:29:01 +0300

    update to more recent emscripten / emcc

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 set -e -x
-export EMCC_DEBUG="0"
+# export EMCC_DEBUG="0"
 rm -rf dist/
 mkdir -p dist
 npm i amdefine
-objext=bc
+objext=o
 emcc -r -O0 -g src/c_array_strings.c src/main.cpp src/myfunc.cpp src/myclass.cpp -Iinclude -o dist/mysqrt."$objext"
-emcc -O0 -g dist/mysqrt."$objext" -o dist/mysqrt.js -s EXPORTED_FUNCTIONS='["_array_free", "_array_print", "_array_set", "_gen_array",  "_main", "_mysqrtC", "_mysqrtArrayC", "_createMyClassC", "_deleteMyClassC", "_setMyClassValueC", "_getMyClassAnswerC"]' -s EXPORTED_RUNTIME_METHODS="['allocate', 'cwrap', 'getValue', 'intArrayFromString', 'setValue', 'ALLOC_STACK', 'FS', 'UTF8ToString']" --memory-init-file 0 --pre-js  wrap/pre.js --post-js wrap/post.js
+emcc -O0 -g dist/mysqrt."$objext" -o dist/mysqrt.js -s EXPORTED_FUNCTIONS='["_array_free", "_array_print", "_array_set", "_gen_array",  "_main", "_mysqrtC", "_mysqrtArrayC", "_createMyClassC", "_deleteMyClassC", "_setMyClassValueC", "_getMyClassAnswerC"]' -s EXPORTED_RUNTIME_METHODS="['allocate', 'cwrap', 'getValue', 'intArrayFromString', 'setValue', 'ALLOC_STACK', 'FS', 'UTF8ToString']" --pre-js  wrap/pre.js --post-js wrap/post.js
 export EMCC_DEBUG=""
 # node --eval $'const m=require("./dist/mysqrt");m().then(()=>{m.array_demo();});'
 node h.js
